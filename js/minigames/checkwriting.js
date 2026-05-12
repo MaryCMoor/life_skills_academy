f365e2d1320107352078471a1011fea44a0a1222
Mary Moor <93663385+MaryCMoor@users.noreply.github.com>
2026-05-12 13:42:13 -0400

    Add files via upload

---
+++ js/minigames/checkwriting.js
@@ -0,0 +1,68 @@
+{
+  "chores": [
+    {
+      "id": "bed",
+      "name": "Make Bed",
+      "time": 10,
+      "reward": 5,
+      "skill": "cleaning",
+      "minAge": 14
+    },
+    {
+      "id": "dishes",
+      "name": "Wash Dishes",
+      "time": 20,
+      "reward": 8,
+      "skill": "cleaning",
+      "minAge": 14
+    },
+    {
+      "id": "vacuum",
+      "name": "Vacuum Living Room",
+      "time": 30,
+      "reward": 10,
+      "skill": "cleaning",
+      "minAge": 14
+    },
+    {
+      "id": "trash",
+      "name": "Take Out Trash",
+      "time": 10,
+      "reward": 5,
+      "skill": "cleaning",
+      "minAge": 14
+    },
+    {
+      "id": "laundry",
+      "name": "Do Laundry",
+      "time": 90,
+      "reward": 12,
+      "skill": "laundry",
+      "minAge": 15
+    },
+    {
+      "id": "mow",
+      "name": "Mow Lawn",
+      "time": 60,
+      "reward": 20,
+      "skill": "cleaning",
+      "minAge": 16
+    },
+    {
+      "id": "bathroom",
+      "name": "Clean Bathroom",
+      "time": 45,
+      "reward": 15,
+      "skill": "cleaning",
+      "minAge": 15
+    },
+    {
+      "id": "dust",
+      "name": "Dust Furniture",
+      "time": 20,
+      "reward": 7,
+      "skill": "cleaning",
+      "minAge": 14
+    }
+  ]
+}
